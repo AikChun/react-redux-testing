91209106d803afd237db9a5f1c6e9ebf360acd96
aikchun <aikchun616@gmail.com>
2017-05-31 01:38:42 +0800

    Added second level tests Added tests on textarea Added tests on submit button

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,15 +4,15 @@ import App from '../../src/components/app';
 
 // use describe to group together similar tests
 describe('App', () => {
+	let component;
 
-  // Use it to test a single attribute of a target
-  it('shows the correct text.', () => {
-    const component = renderComponent(App);
-    // use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter')
+	beforeEach(() => {
+		component = renderComponent(App);
+	})
 
-  });
-
+	it('shows a comment box', () => {
+		expect(component.find('.comment-box')).to.exist;
+	})
 
 
 });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -19,4 +19,21 @@ describe('', () => {
 	it('has a button', () => {
 		expect(component.find('button')).to.exist;
 	});
+
+	describe('entering some text', () => {
+
+		beforeEach(() => {
+			component.find('textarea').simulate('change', 'new comment');
+		});
+
+		it('shows text in the textarea', () => {
+			expect(component.find('textarea')).to.have.value('new comment');
+		});
+
+		it('when submitted, clears the input', () => {
+			component.simulate('submit');
+			expect(component.find('textarea')).to.have.value('');
+		});
+
+	});
 });
