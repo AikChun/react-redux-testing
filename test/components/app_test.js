b5e4ee0790c1e0841d8d6adddaeac78788c5af4d
aikchun <aikchun616@gmail.com>
2017-05-30 20:03:41 +0800

    Added some basic tests for react using chai-jquery

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,18 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
+// use describe to group together similar tests
+describe('App', () => {
+
+  // Use it to test a single attribute of a target
+  it('shows the correct text.', () => {
+    const component = renderComponent(App);
+    // use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter')
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+
+
 });
