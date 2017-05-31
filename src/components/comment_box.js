cac9259a3e9c10c7f33d1188546b17e83e2f176a
aikchun <aikchun616@gmail.com>
2017-06-01 00:40:22 +0800

    Completed React Redux Chai jQuery testing

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
-export default class CommentBox extends Component {
+import { connect } from 'react-redux';
+import * as actions from '../actions/index';
+class CommentBox extends Component {
 	constructor(props) {
 		super(props);
 
@@ -14,16 +15,23 @@ export default class CommentBox extends Component {
 	handleSubmit(event) {
 		event.preventDefault();
 
+        this.props.saveComment(this.state.comment);
 		this.setState({ comment: '' });
 	}
 
-	
+
 	render() {
 		return (
 			<form onSubmit={ this.handleSubmit.bind(this) } className="comment-box">
+                <h4> Add a comment </h4>
 				<textarea value={ this.state.comment } onChange={ this.handleChange.bind(this) } />
-				<button action="submit">Submit Commment</button>
+                <div>
+                    <button action="submit">Submit Commment</button>
+                </div>
+
 			</form>
 		);
 	}
 }
+
+export default connect(null, actions)(CommentBox);
