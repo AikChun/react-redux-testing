c8afae47c62edad55f6b1df80684b69513193e83
aikchun <aikchun616@gmail.com>
2017-05-31 08:12:30 +0800

    Added comment list component added comment list test

---
+++ src/components/comment_list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CommentList extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<ul className="comment-list">
+			
+			</ul>
+		);
+	}
+}
+
+export default CommentList;
